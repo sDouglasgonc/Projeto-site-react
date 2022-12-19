c02f0229afea71b915e24aa26a326fc2b4056417
sDouglasgonc <sdouglas968@gmail.com>
2022-12-19 20:31:59 -0300

    home feita e o caminho do new project
    alem dos css e a navbar e o footer

--- costs/src/App.js
+++ costs/src/App.js
@@ -1,28 +1,27 @@
-import {BrowserRouter as  Router, Routes, Route,Link} from 'react-router-dom'
+import {BrowserRouter as  Router, Routes, Route} from 'react-router-dom'
 import Home from './components/pages/Home';
 import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
 import Container from './components/layout/Container';
 import NewProject from './components/pages/NewProject';
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
+import Projects from './components/pages/Projects';
 
 function App() {
   return (
     <Router>
-<div>
-  <Link to='/'>Home</Link>
-  <Link to='/contact'>Contato</Link>
-  <Link to='/company'>Empresa</Link>
-  <Link to='/newproject'>Novo Projeto</Link>
-</div>
+<Navbar/>
 <Container customClass='min-height'>
 <Routes>
   <Route  path='/' exact='true' element={<Home/>}></Route>
+  <Route path='/projects' exact='true' element={<Projects/>}/>
   <Route  path='/company' element={<Company/>}></Route>
   <Route  path='/contact' element={<Contact/>}></Route>
   <Route  path='/newproject' element={<NewProject/>}></Route>
 </Routes>
 </Container>
-<p>Footer</p>
+<Footer/>
 
     </Router>
   );
---
+++ costs/src/components/pages/Projects.js
@@ -0,0 +1,8 @@
+import React from "react"
+function Projects(){
+    return(
+<h1>Projetos</h1>
+    )
+}
+
+export default Projects
