3558647407912b062d2b7b00ad8bb3f5b97181f5
sDouglasgonc <sdouglas968@gmail.com>
2022-12-20 18:18:10 -0300

    form simples do projeto

--- costs/src/components/pages/NewProject.js
+++ costs/src/components/pages/NewProject.js
@@ -1,7 +1,14 @@
+import ProjectForm from '../project/ProjectForm'
+import styles from './NewProject.module.css'
 import React from 'react'
 function NewProject(){
     return(
-<h1>Novo Projeto</h1>
+        <div className={styles.newproject_container}>
+<h1>Criar Projeto</h1>
+<p>Crie seu projeto para depois adicionar os serviços</p>
+<ProjectForm/>
+        </div>
+
     )
 }
 
