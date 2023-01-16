ca8db5e6bca4b4a5baa49c993e778b36780d7ce6
sDouglasgonc <sdouglas968@gmail.com>
2023-01-16 19:28:28 -0300

    pagina de edicao e css da mesma

--- costs/src/components/pages/Project.js
+++ costs/src/components/pages/Project.js
@@ -1,13 +1,16 @@
-//import styles from './Project.module.css'
+import styles from './Project.module.css'
 import{useParams} from 'react-router-dom'
 import {useState, useEffect} from 'react'
-
+import Loading from '../layout/Loading'
+import Container from '../layout/Container'
 function Project(){
 const {id} = useParams()
 
 const [project, setProject] = useState([])
+const [showProjectForm , setShowProjectForm] = useState(false)
 
 useEffect(()=>{
+   setTimeout(()=>{
     fetch(`http://localhost:5000/projects/${id}`,{
         method:'GET',
         headers:{
@@ -18,9 +21,47 @@ useEffect(()=>{
         setProject(data)
     })
     .catch((err) => console.log(err))
+   },5000)
 }, [id])
 
-    return <p>{project.name}</p>
+function toogleProjectForm (){
+setShowProjectForm(!showProjectForm)
+}
+
+    return (
+    <> 
+    {project.name ? (
+        <div className={styles.project_details}>
+        <Container customClass='column'>
+            <div className={styles.details_container}>
+                <h1>Projeto: {project.name}</h1>
+                <button className={styles.btn} onClick={toogleProjectForm}>
+                {!showProjectForm ? 'Editar projeto':'Fechar'}
+                </button>
+                {!showProjectForm ?(
+                <div className={styles.project_info} >
+                    <p>
+                    <span>Categoria:</span> {project.category.name}
+                    </p>
+                    <p>
+                    <span>Total do Orçamento:</span> R${project.budget}
+                    </p>
+                    <p>
+                    <span>Total ultilizado:</span> R${project.cost}
+                    </p>
+                </div>
+                ):(
+                <div className={styles.project_info}>
+                    <p>detalhes do projeto</p>
+                </div>
+                )}
+            </div>
+        </Container>
+        </div>
+    ):(
+        <Loading/>
+    )}
+    </> )
     
 }
 export default Project
