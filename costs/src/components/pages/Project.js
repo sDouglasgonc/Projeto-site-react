4ab35d74a4f2e979e0f361f7ee004c81a2681f1f
sDouglasgonc <sdouglas968@gmail.com>
2023-01-17 19:59:24 -0300

    edição de formulario e mensagem

--- costs/src/components/pages/Project.js
+++ costs/src/components/pages/Project.js
@@ -3,11 +3,17 @@ import{useParams} from 'react-router-dom'
 import {useState, useEffect} from 'react'
 import Loading from '../layout/Loading'
 import Container from '../layout/Container'
+import ProjectForm from '../project/ProjectForm'
+import Message from '../layout/Message'
+
 function Project(){
 const {id} = useParams()
 
 const [project, setProject] = useState([])
 const [showProjectForm , setShowProjectForm] = useState(false)
+const [showServiceForm , setShowServiceForm] = useState(false)
+const [message , setmessage] = useState()
+const [type, setType] = useState()
 
 useEffect(()=>{
    setTimeout(()=>{
@@ -21,18 +27,49 @@ useEffect(()=>{
         setProject(data)
     })
     .catch((err) => console.log(err))
-   },5000)
+   },500)
 }, [id])
+
+function editPost(project){
+setmessage('')
+
+//budget validation
+if(project.budget < project.cost){
+  setmessage('O orçamento não pode ser menor que o custo do projeto!')
+  setType('error')
+  return(false)
+}
+fetch(`http://localhost:5000/projects/${project.id}`,{
+    method:'PATCH',
+    headers: {
+        'Content-Type':'application/json',
+    },
+    body: JSON.stringify(project),
+})
+.then((resp)=> resp.json())
+.then((data)=>{
+    setProject(data)
+    setShowProjectForm(false)
+    setmessage('Projeto atualizado!')
+    setType('success')
+})
+.catch((err)=> console.log(err))
+}
 
 function toogleProjectForm (){
 setShowProjectForm(!showProjectForm)
 }
+
+function toogleServiceForm (){
+    setShowServiceForm(!showServiceForm)
+    }
 
     return (
     <> 
     {project.name ? (
         <div className={styles.project_details}>
         <Container customClass='column'>
+            {message && <Message type={type} msg={message}/>}
             <div className={styles.details_container}>
                 <h1>Projeto: {project.name}</h1>
                 <button className={styles.btn} onClick={toogleProjectForm}>
@@ -52,10 +89,28 @@ setShowProjectForm(!showProjectForm)
                 </div>
                 ):(
                 <div className={styles.project_info}>
-                    <p>detalhes do projeto</p>
+                    <p><ProjectForm
+                    handleSubmit={editPost}
+                    btnText='Concluir edição'
+                    projectData={project}
+                    /></p>
                 </div>
                 )}
             </div>
+
+            <div className={styles.service_form_container}>
+                    <h2>Adicione um serviço:</h2>
+                    <button className={styles.btn} onClick={toogleServiceForm}>
+                {!showServiceForm ? 'Adicionar Serviço':'Fechar'}
+                </button>
+            <div className={styles.project_info}>
+                {showServiceForm && <div>formulario do serviço </div> }
+            </div>   
+            </div>
+            <h2>Serviços</h2>
+            <Container customClass='start'>
+                <p>Itens de serviços</p>
+            </Container>
         </Container>
         </div>
     ):(
