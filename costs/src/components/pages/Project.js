a8a3012cc3e719dc4afe67f500a15bba4d1e3609
sDouglasgonc <sdouglas968@gmail.com>
2023-01-19 20:02:13 -0300

    configurando os servicos e excluindo

--- costs/src/components/pages/Project.js
+++ costs/src/components/pages/Project.js
@@ -7,11 +7,13 @@ import Container from '../layout/Container'
 import ProjectForm from '../project/ProjectForm'
 import Message from '../layout/Message'
 import ServiceForm from '../service/ServiceForm'
+import ServiceCard from '../service/ServiceCard'
 
 function Project(){
 const {id} = useParams()
 
 const [project, setProject] = useState([])
+const [services, setServices] = useState([])
 const [showProjectForm , setShowProjectForm] = useState(false)
 const [showServiceForm , setShowServiceForm] = useState(false)
 const [message , setMessage] = useState()
@@ -27,6 +29,7 @@ useEffect(()=>{
     }).then(resp => resp.json())
     .then((data)=>{
         setProject(data)
+        setServices(data.services)
     })
     .catch((err) => console.log(err))
    },500)
@@ -92,8 +95,35 @@ fetch(` http://localhost:5000/projects/${project.id}`,{
 .then((resp) => resp.json())
 .then((data)=>{
 console.log(data)
-//exibir serviços
-}) .catch((err) => console.log(err))
+
+}) 
+.catch((err) => console.log(err))
+}
+
+function removeService (id, cost ){
+const servicesUpdated = project.services.filter(
+    (service) => service.id !== id
+)
+
+const projectUpdated = project
+
+projectUpdated.services = servicesUpdated
+projectUpdated.cost = parseFloat(projectUpdated.cost) - parseFloat(cost)
+
+fetch(`http://localhost:5000/projects/${projectUpdated.id}`,{
+    method: 'PATCH',
+    headers:{
+        'Content-Type':'application/json'
+    },
+    body: JSON.stringify(projectUpdated),
+})
+.then((resp) => resp.json())
+.then((data) => {
+setProject(projectUpdated)
+setServices(servicesUpdated)
+setMessage('Serviço removido com sucesso!')
+})
+.catch((err) => console.log(err))
 }
 
 function toogleProjectForm (){
@@ -154,7 +184,19 @@ function toogleServiceForm (){
             </div>
             <h2>Serviços</h2>
             <Container customClass='start'>
-                <p>Itens de serviços</p>
+                {services.length > 0 && 
+                services.map((service) =>(
+                    <ServiceCard
+                    id={service.id}
+                    name={service.name}
+                    cost={service.cost}
+                    description={service.description}
+                    key={service.id}
+                    handleRemove={removeService}
+                    />
+                ))
+            }
+                {services.length === 0 &&  <p>Não há serviços cadastrados.</p>}
             </Container>
         </Container>
         </div>
