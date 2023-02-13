567b1334a2f449cb80f2a7c5b0d115993495b5fb
sDouglasgonc <sdouglas968@gmail.com>
2023-02-13 18:36:14 -0300

    nada

--- costs/src/components/project/ProjectForm.js
+++ costs/src/components/project/ProjectForm.js
@@ -64,7 +64,7 @@ function ProjectForm({ handleSubmit, btnText, projectData }) {
         text="Selecione a categoria"
         options={categories}
         handleOnChange={handleCategory}
-        value={project.category ? project.category.id : ''}
+        vale={project.category ? project.category.id : ''}
       />
       <SubmitButton text={btnText} />
     </form>
